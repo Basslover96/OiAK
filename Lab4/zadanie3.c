ce15590d341a91af61f7335ad1a0cd17fc6be72b
Basslover96 <dobrysiewiczk@interia.pl>
2019-05-12 18:53:06 +0200

    Dokończone laboratororium 4

--- Lab4/zadanie3.c
+++ Lab4/zadanie3.c
@@ -7,31 +7,30 @@ int modifier = 3;
 int main(){
 
     asm(
-    "xor %%r8, %%r8\n"
+    "mov $0, %%rcx\n"
     "xor %%rbx, %%rbx\n"
-    "movq $10, %%r10\n"
+    "mov $10, %%r10\n"
     "loop:\n"
-    "movb (%0, %%r8, 1), %%bl\n"
-    "subb $'0', %%bl\n"
-    "cmpb $0, %%bl\n"
+    "mov (%0, %%rcx, 1), %%bl\n"
+    "sub $'0', %%bl\n"
+    "cmp $0, %%bl\n"
     "jl save\n"
-    "cmpb $9, %%bl\n"
+    "cmp $9, %%bl\n"
     "jg save\n"
-    "add %2, %%rbx\n"
+    "add modifier, %%rbx\n"
     "xor %%rdx, %%rdx\n"
-    "movq %%rbx, %%rax\n"
-    "movq %%r10, %%rcx\n"
-    "divq %%r10\n"
-    "movq %%rdx, %%rbx\n"
-    "addb $'0', %%bl\n"
+    "mov %%rbx, %%rax\n"
+    "div %%r10\n"
+    "mov %%rdx, %%rbx\n"
+    "add $'0', %%bl\n"
     "save:\n"
-    "movb %%bl, (%0, %%r8, 1)\n"
-    "incq %%r8\n"
-    "cmp %1, %%r8\n"
+    "mov %%bl, (%0, %%rcx, 1)\n"
+    "inc %%rcx\n"
+    "cmp test_len, %%ecx\n"
     "jne loop\n"
     :
-    :"r"(&test_string), "r"(test_len), "r"(modifier)
-    :"%rax","%rbx","%rdx","%r8","%r10");
+    :"D"(test_string)
+    :"%rax","%rbx","%rcx","%rdx","%r8","%r10");
     printf("The result string is: %s\n", test_string);
     return 0;
 }
